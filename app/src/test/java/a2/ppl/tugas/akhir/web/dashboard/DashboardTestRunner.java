1b04508a3e36962bb0934d6b0ab7831fe89af55b
NItroF77 <142875762+RaihanFuadSyakir@users.noreply.github.com>
2024-06-03 09:51:08 +0700

    add test runner

--- app/src/test/java/a2/ppl/tugas/akhir/web/dashboard/DashboardTestRunner.java
+++ app/src/test/java/a2/ppl/tugas/akhir/web/dashboard/DashboardTestRunner.java
@@ -1,4 +1,4 @@
-package a2.ppl.tugas.akhir.web.checkout;
+package a2.ppl.tugas.akhir.web.dashboard;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -10,8 +10,8 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("a2.ppl.tugas.akhir.web.checkout")
+@SelectPackages("a2.ppl.tugas.akhir.web.dashboard")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "a2.ppl.tugas.akhir.web.checkout")
-public class CucumberTestRunner {
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "a2.ppl.tugas.akhir.web.dashboard")
+public class DashboardTestRunner {
 }
